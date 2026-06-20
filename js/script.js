146f4e9ca165cb9805bd083e94e7c279942ffeff
MAICKON MENDES <maickonmmendes@gmail.com>
2026-06-20 19:21:57 -0300

    seleção area de compra

--- js/script.js
+++ js/script.js
@@ -1,16 +1,24 @@
+// ativar links do menu
 const links = document.querySelectorAll(".header-menu a");
-
 function ativarLink(link) {
   const url = location.href; //pega a url do site
-  const href = link.href;  //pega o caminho das pgs
-
-//Includes é uma propriedade que confere se existe elemantos na string, exemplo, javaScript.includes("java") retorn true
-  if (url.includes(href)) {  
-    link.classList.add("ativo")
+  const href = link.href; //pega o caminho das pgs
+  //Includes é uma propriedade que confere se existe elemantos na string, exemplo, javaScript.includes("java") retorn true
+  if (url.includes(href)) {
+    link.classList.add("ativo");
   }
-  console.log(url)
-  console.log(href)
-
 }
+links.forEach(ativarLink);
 
-links.forEach(ativarLink)
+// ativar itens do orçamento
+
+//location é um objetto com varios parametros dentro. o .search é um dos elementos dentro desse objeto que da acesso ao caminho que foi colocado depois do ?
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+parametros.forEach(ativarProduto);
