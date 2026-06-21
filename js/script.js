ad964a758dc9ddf746ec227caebc3c04653dacbf
MAICKON MENDES <maickonmmendes@gmail.com>
2026-06-21 10:59:58 -0300

    plugin de animação

--- js/script.js
+++ js/script.js
@@ -22,3 +22,45 @@ function ativarProduto(parametro) {
   }
 }
 parametros.forEach(ativarProduto);
+
+// Perguntas Frequentes
+const perguntas = document.querySelectorAll(".perguntas button");
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+perguntas.forEach(eventosPerguntas);
+
+//galeria imagens
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
+// o matchMedia é um metodo que avalia o tamanho da tela e aplica uma função conforme é determinada o tamanho da tela
+// o prepend remove um item da array e coloca de novo em primeiro lugar na lista
+
+/*Animação */
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
